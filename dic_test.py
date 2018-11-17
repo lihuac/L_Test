468ce84c6ae05884d165d06677b7c90d824fc291
lihuac <lyohhwa@gmail.com>
2018-11-16 18:06:26 -0800

    add print message

--- dic_test.py
+++ dic_test.py
@@ -1,33 +1,194 @@
-
+#import serial
 import re
+import os
+import datetime
+import time
+import subprocess
 
 testlist_file = 'part2a_testlist.txt'
+serial_number = '1234567'
 testcfg_file  = 'test.cfg'
+templogfile   = 'temp.log'
+directory     = 'log'
+test_time     = str(datetime.datetime.now())[:19]
+SERIAL_CMD    = 1
+LINUX_CMD     = 0
 
-dic_config = {}
-test_array = []
+dic_config    = {}
+test_array    = []
+
+LOG           = open(templogfile, "w")
+
 
 class Test:
 	test_description = ""
-	test_path = ""
-	command = ""
-	pass_phrase = ""
-	fail_phrase = ""
-	timeout = 0
-	retry = 0
+	test_path        = ""
+	command          = ""
+	pass_phrase      = ""
+	fail_phrase      = ""
+	timeout          = 0
+	retry            = 0
 
 # Test MAIN	
 def main():
+	#create_log_dir(directory)
 	read_config()
 	#printing_testcfg()
 	tmp = ''
 	parse_testlist(testlist_file, tmp)
 	printing()
+	#create_sn_file()
+	initiate_logging()
+	run_testlist()
+	LOG.close()
+	
+
+#	my $status = check_result($test_item[$line]{'test_description'},
+#							  $test_item[$line]{'pass_phrase'}, 
+#							  $test_item[$line]{'fail_phrase'}, 
+#							  $test_item[$line]{'timeout'},1, \$port{$testpath});
+
+	
+def execute_linux_cmd(test_index):
+	console_log_print("Linux Test for test item " + str(test_index) + "\n\n")
+	return
+	path = test_array[test_index].test_path
+	cmd = test_array[test_index].command
+	status = 1
+	print "start linux command"
+	if path != '':
+		# change directory
+		print "change directory"
+	if cmd != '':
+		# cmd
+		cmd = 'dir'
+		print "command is "+cmd
+		res = ''
+		res = subprocess.check_result(cmd)
+		print "res is " + res
+		#status = check_result(test_index, LINUX_CMD, 0)
+	else:
+		status = check_result(test_index, LINUX_CMD, 2)
+		
+	print ("linux: " + path+" " +cmd)
+	
+def execute_serial_cmd(test_index):
+	console_log_print("Serial Test for test item " + str(test_index) + "\n\n")
+
+def check_result(index, cmd_type, ref_port):
+	name       = test_array[index].test_description
+	pass_word  = test_array[index].pass_phrase
+	fail_word  = test_array[index].fail_phrase
+	time_out   = test_array[index].time_out
+	retry_time = test_array[index].retry
+	result      = 0
+	res_content = ''
+	output      = ''
+	while(retry_time >= 0):
+		start_time      = datetime.datetime.now()
+		start_seconds   = time.mktime(start_time.timetuple())
+		current_time    = datetime.datetime.now()
+		current_seconds = time.mktime(current_time.timetuple())
+		while(1):							# 
+			if time_out > 0 and current_seconds-start_seconds > time_out:
+				print "time over"
+				console_log_print("TIMEOUT")
+				break
+			time.sleep(1)
+			
+			if cmd_type == SERIAL_CMD:		# serial port command
+				print "serial"
+				#_____ update res_content here
+				
+			elif cmd_type == LINUX_CMD:		# linux port command
+				print "linux"
+				#_____ update res_content here
+			else:
+				print "wrong cmd type"
+				
+			output = output + res_content
+			console_log_print(res_content)
+		
+			if pass_word != '':
+				if pass_word in output:
+					result = 1
+					break
+			
+			if fail_word != '':
+				if fail_word in output:
+					time_out = 0
+					break
+					
+			current_time = datetime.datetime.now()
+			current_seconds = time.mktime(current_time.timetuple())
+			# Ending ...
+		if result == 1:
+			break
+		console_log_print("NONO Retrying...");
+		console_log_print("Remaining Retry: "+ retry_time +"\n\n");
+		retry_time = retry_time - 1
+		# Ending retry ...
+	
+	console_log_print("\n################################################################\n");	
+	console_log_print(" " + name + " RESULT:\n");
+	console_log_print("################################################################\n");
+	if result == 1:
+		console_log_print("OKOK\n")
+		return 0
+	else:
+		console_log_print("NONO\n")
+		return 1
 	
 	
 def run_testlist():
-	print ("running")
+	for i in range(len(test_array)):
+		#print i
+		console_log_print("################################################################\n");	
+		console_log_print(test_array[i].test_description + " search for:  " +
+		                  "\"" +test_array[i].pass_phrase + "\" in " +
+		                  test_array[i].timeout + " seconds.\n");
+		console_log_print("################################################################\n");
+		# ttyUSB
+		if "ttyUSB" in test_array[i].test_path:
+			execute_serial_cmd(i)
+			
+		else:
+			execute_linux_cmd(i)
+			
+def init_serial_port(test_index):
+	path = test_array[test_index].test_path
+	ser = serial.Serial(
+		port='/dev/ttyUSB0', 
+		baudrate=115200, 
+		#timeout=1,
+		parity=serial.PARITY_NONE,
+		stopbits=serial.STOPBITS_ONE,
+		bytesize=serial.EIGHTBITS,
+		rtscts=False
+	)
 	
+def create_sn_file():
+	sn_filename = 'sn.txt'
+	SN = open(sn_filename, "w");
+	SN.write(serial_number);
+	SN.close()
+	
+def initiate_logging():
+	#print ("created LOG.")
+	console_log_print ("#########################################################################\n");	
+	console_log_print ("       Board Serial Number: " + serial_number + "                                        \n");
+	console_log_print ("       Test Date and Time:  " + test_time +"                         \n");
+	
+	
+def console_log_print(log):
+	LOG.write(log)
+	print (log)
+	
+def create_log_dir(directory):
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+	
+
 def read_config():
 	with open (testcfg_file, 'r') as F:
 		for row in F:
